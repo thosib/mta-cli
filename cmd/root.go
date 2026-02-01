f60631723a5766237f9f483e45c7fe8e85b47bc6
Thomas <thomas@sibilly.com>
2026-01-31 23:20:51 -0500

    init project w/Cobra

---
+++ cmd/root.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "mta-cli",
+	Short: "NYC MTA real-time subway information CLI",
+	Long: `mta-cli provides real-time arrival information for the NYC Subway.
+Currently supports lines 1, 2, and 3 (A Division - IRT).`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Define persistent flags for the root command
+	// These will be available to all subcommands
+}
+
